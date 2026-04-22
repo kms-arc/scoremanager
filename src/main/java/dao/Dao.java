98c0564f5332d54680b81d36055f87cd8f354667
m_shimada <m_shimada@10.251.241.192>
2026-04-22 12:44:15 +0900

    dao

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -1,16 +1,19 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 
-public class Dao {
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
-    protected Connection getConnection() throws Exception {
-        Class.forName("org.h2.Driver");
-        return DriverManager.getConnection(
-            "jdbc:h2:file:~/kaihatu",  // ← H2 の DB ファイル名
-            "sa",                      // ← ユーザー名
-            ""                         // ← パスワード（空）
-        );
-    }
+public class Dao{
+	static DataSource ds;
+	
+	public Connection getConnection() throws Exception{
+		if (ds==null) {
+			InitialContext ic =  new InitialContext();
+			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/kaihatu");
+		}
+		return ds.getConnection();
+	}
 }
+
